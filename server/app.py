10fecce58aedf3ca53bb44fd26a65e5bdb8ee170
Clay Mangum <clay@trinitasaccounting.com>
2024-04-02 14:53:37 -0400

    completed

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@
 
 from flask import Flask, make_response
 from flask_migrate import Migrate
+from sqlalchemy_serializer import SerializerMixin
 
 from models import db, Earthquake
 
@@ -21,6 +22,32 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+
+@app.route('/earthquakes/<int:id>')
+def get_earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+
+    if earthquake:
+        body = earthquake.to_dict()
+        status = 200
+
+    else:
+        body = {'message' : f'Earthquake {id} not found.'}
+        status = 404
+
+    return make_response(body, status)
+
+
+
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquakes_by_minimum_magnitude(magnitude):
+    earthquakes = []
+    for eq in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all():
+        earthquakes.append(eq.to_dict())
+    
+    body = {'count': len(earthquakes), 'quakes': earthquakes}
+    return make_response(body, 200)
 
 
 if __name__ == '__main__':
